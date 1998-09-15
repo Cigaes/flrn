14069d95833bc5c58252dee0264dc798da15c78c
jo <jo>
1998-09-15 20:45:05 +0000

    Suppression de config.h et pathdef.h de l'archive.
    			Jo.

--- src/pathdef.h
+++
@@ -1,4 +0,0 @@
-/* This file is automatically created by Makefile
- * DO NOT EDIT!  Change Makefile only. */
-#define PATH_HELP_DIRECTORY "/usr/local/util/lib/flrn/help/"
-#define PATH_CONFIG_DIRECTORY "/usr/local/util/lib/flrn/"
