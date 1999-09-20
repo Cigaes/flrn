1c9e424d5d644defee4cd7c22460784c42865132
damien <damien>
1999-09-20 08:32:43 +0000

    Mieux...

--- src/flrn_help.c
+++ src/flrn_help.c
@@ -26,19 +26,19 @@ void Aide () {
    FILE *file;
    char key='m';
 
-   num_help_line=4;
    while (key!='q') {
       file=open_flhelpfile(key);
       key='\0';
       if (file==NULL) { 
+         num_help_line=16;
          Aff_error("Erreur : ne trouve pas le fichier d'aide cherché."); 
 	 if (key=='m') return; 
       } else {
          key=Aff_file(file,hl_chars,hl_mesgs);
          fclose(file);
+         num_help_line=16;
+         Aff_help_line(Screen_Rows-1);
       }
-      num_help_line=16;
-      Aff_help_line(Screen_Rows-1);
       if (key==0) Aff_fin(hl_mesgs);
       while ((key==0) || (!strchr(hl_chars,key))) {
          key=(char)Attend_touche();
