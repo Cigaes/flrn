89dea596dc90de806151547bd92572d89d2fedec
damien <damien>
1999-12-02 10:54:27 +0000

    Ok.

--- src/site_config.h
+++ src/site_config.h
@@ -12,7 +12,7 @@
 #ifdef IN_OPTION_C
 
 struct Option_struct Options = {
-  "news.rezo.ens.fr",  		 /* server */
+  "news",  		 /* server */
   DEFAULT_NNTP_PORT,    /* port (DEFAULT_NNTP_PORT = 119) */
   NULL,			/* flnews_ext */
   NULL, 		/* Post name - doit rester NULL */
@@ -68,10 +68,10 @@ struct Option_struct Options = {
   NULL,			/* help_line_file */
   NULL,			/* auth_user */
   NULL,			/* auth_pass */
-  NULL,			/* sign_file */
+  NULL,			/* sig_file */
   0,			/* quote_all */
 #ifdef WITH_CHARACTER_SETS
-  NULL,			/* character_set */
+  NULL,                 /* character_set */
 #endif
   };
 
