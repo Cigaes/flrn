a7524ef99b5372d680308d362a43db8e10ba28a2
masse <masse>
2003-10-11 10:58:33 +0000

    bugfix

--- src/enc/enc_strings.c
+++ src/enc/enc_strings.c
@@ -1291,11 +1291,11 @@ int convert_termchar (const char *str, size_t len, flrn_char **res, size_t lb) {
 	    return 1;
 	} else return 1;
     } else if (rc==(size_t)(-3)) {
-	rc=UCGetUniFromUtf8String(NULL,str,strlen(str));
+	rc=UCGetUniFromUtf8String(NULL,ptr,strlen(ptr));
 	if ((int)rc==(-1)) return -1;
 	if ((int)rc==(-2)) return 0;
 	if (rc>lb) {
-	    *res=safe_strdup(str);
+	    *res=safe_strdup(ptr);
 	} else fl_strncpy(*res,ptr,lb);
 	return lb;
     } else if (rc==(size_t)(-1)) {
